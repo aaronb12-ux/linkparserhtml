2ac6f7c92a270aa66088f5401222cd517dea8559
aaronb12-ux <afairster03@gmail.com>
2025-11-23 12:59:13 -0800

    added some more logic to link parsing...

--- main.go
+++ main.go
@@ -8,24 +8,52 @@ import (
 	"golang.org/x/net/html"
 )
 
-
-
+type Link struct {
+	Href string
+	Text []string
+}
 
 func main() {
 
-	f, err := os.Open("ex1.html")
+	f, err := os.Open("ex2.html")
 
 	if err != nil {
 		log.Fatalf("unable to open file")
 	}
 
+	var Links []Link
+
 	doc, err := html.Parse(f)
 
 	for n := range doc.Descendants() {
-		if n.Type == html.ElementNode && n.Data == "href /other-page"{
-			fmt.Println(n.Data)
-		}
+
+		
+		fmt.Println("the element node is", html.ElementNode)
+		fmt.Println("the n.data is", n.Data)
+		fmt.Println("the attribute is", n.Attr)
+		fmt.Println("the type is", n.Type)
+		fmt.Println()
+
+
+		if n.Data == "a"{ //need to find a links then search within those a links...
+			
+			var link Link
+			if len(n.Attr[0].Val) != 0 {
+				
+				l := n.Attr[0].Val
+				link.Href = l
+
+			}
+			//now search within this node?
+			fmt.Println(n.Type, html.ElementNode)
+			if n.Type == 3 && html.ElementNode == 3 {
+				link.Text = append(link.Text, n.Data)
+			}
+
+			Links = append(Links, link)
+		}	
 	}
 
-	
+	fmt.Println(Links)
+
 }
